8e5f0105ba61b41549472f60857fc5169418eb26
Scott Payne <elance@scottpayne.com.au>
2014-06-23 12:15:39 +1000

    Horrible hack to get drupalextension working with Domain Access

--- src/Drupal/Driver/Cores/Drupal7.php
+++ src/Drupal/Driver/Cores/Drupal7.php
@@ -35,11 +35,11 @@ class Drupal7 implements CoreInterface {
     // Bootstrap Drupal.
     $current_path = getcwd();
     chdir(DRUPAL_ROOT);
-    drupal_bootstrap(DRUPAL_BOOTSTRAP_CONFIGURATION);
+    drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
     if (empty($GLOBALS['databases'])) {
       throw new BootstrapException('Missing database setting, verify the database configuration in settings.php.');
     }
-    drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
+    // drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
     chdir($current_path);
   }
 
